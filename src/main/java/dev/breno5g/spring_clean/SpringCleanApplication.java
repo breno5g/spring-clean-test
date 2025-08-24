f4fdf037eede12f25b64bdcf63b01ba49ed3c787
Breno Santos <brenosantos145@gmail.com>
2025-08-24 00:47:45 -0300

    first commit

---
+++ src/main/java/dev/breno5g/spring_clean/SpringCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.breno5g.spring_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/breno5g/spring_clean/SpringCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.breno5g.spring_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
