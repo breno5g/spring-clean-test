7815cbe77333420e309da29b213d9694c82a126a
Breno Santos <brenosantos145@gmail.com>
2025-08-24 00:54:09 -0300

    feat: basic event clas

---
+++ src/main/java/dev/breno5g/spring_clean/core/entities/Event.java
@@ -0,0 +1,18 @@
+package dev.breno5g.spring_clean.core.entities;
+
+import dev.breno5g.spring_clean.core.enums.EventType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Event(
+        UUID id,
+        String name,
+        String description,
+        LocalDateTime initialDate,
+        LocalDateTime finalDate,
+        String location,
+        Integer capacity,
+        EventType type
+) {
+}
---
+++ src/main/java/dev/breno5g/spring_clean/core/enums/EventType.java
@@ -0,0 +1,8 @@
+package dev.breno5g.spring_clean.core.enums;
+
+public enum EventType {
+    CONCERT,
+    MOVIE,
+    MUSICAL,
+    OTHER
+}
