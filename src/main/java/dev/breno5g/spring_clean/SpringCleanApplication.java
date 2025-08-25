993af6590c0b112a60eed92111a48ab47591ab1b
Breno Santos <brenosantos145@gmail.com>
2025-08-24 22:37:24 -0300

    feat: integrate dotenv for environment variable management

--- src/main/java/dev/breno5g/spring_clean/SpringCleanApplication.java
+++ src/main/java/dev/breno5g/spring_clean/SpringCleanApplication.java
@@ -1,13 +1,18 @@
 package dev.breno5g.spring_clean;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCleanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCleanApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(
+                e -> System.setProperty(e.getKey(), e.getValue())
+        );
+        SpringApplication.run(SpringCleanApplication.class, args);
 	}
 
 }
